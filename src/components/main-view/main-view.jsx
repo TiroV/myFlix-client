52204c128d1480b4677092c356bd0d790218efbc
TiroV <91702935+TiroV@users.noreply.github.com>
2022-01-25 17:14:05 -0800

    Added button functionality, fixed parcel version, added descriptions
    When committed, adds button functionality, adds main-view and movie card. Updated package.json to roll back parcel version, added pictures and a description to accompany the three movies displayed, and updated some other code to run more smoothly.

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,16 +1,45 @@
 import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
+import { MovieView } from '../movie-view/movie-view';
 
 class MainView extends React.Component {
 
+
+    constructor() {
+        super();
+        this.state = {
+            movies: [
+                { _id: 1, Title: 'Ponyo', Description: 'It\'s ponyo! ', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/9/9d/Ponyo_%282008%29.png' },
+                { _id: 2, Title: 'Akira', Description: 'Destruction follows in the wake of Tetsuo as he searches for answers on the mysterious Akira.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5d/AKIRA_%281988_poster%29.jpg' },
+                { _id: 3, Title: 'Dune', Description: 'Based on the science fiction novel of the same name, set on the planet of Arrakis. ', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/8/8e/Dune_%282021_film%29.jpg' }
+            ],
+            selectedMovie: null
+        }
+    }
+
+    setSelectedMovie(newSelectedMovie) {
+        this.setState({
+            selectedMovie: newSelectedMovie
+        });
+    }
+
     render() {
+        const { movies, selectedMovie } = this.state;
+
+
+        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
         return (
             <div className="main-view">
-                <div>Ponyo</div>
-                <div>Akira</div>
-                <div>Dune</div>
+                {selectedMovie
+                    ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
+                    : movies.map(movie => (
+                        <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }} />
+                    ))
+                }
             </div>
         );
     }
-}
 
+}
 export default MainView;
--- src/main-view.jsx
+++ src/main-view.jsx
@@ -1,16 +1,29 @@
 import React from 'react';
+import { MovieCard } from '../movie-card/movie-card';
 
-class MainView extends React.Component {
+export class MainView extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            movies: [
+                { _id: 1, Title: 'Ponyo1', Description: 'desc1...', ImagePath: '...' },
+                { _id: 2, Title: 'Akira', Description: 'desc2...', ImagePath: '...' },
+                { _id: 3, Title: 'Dune', Description: 'desc3...', ImagePath: '...' }
+            ]
+        }
+    }
 
     render() {
+        const { movies } = this.state;
+
+        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+
         return (
             <div className="main-view">
-                <div>Ponyo</div>
-                <div>Akira</div>
-                <div>Dune</div>
+                {movies.map(movie => <MovieCard />)}
             </div>
         );
     }
-}
 
 export default MainView;
