dfc1d9a2e304daf8b1b06cc7781975667c5e470f
TiroV <91702935+TiroV@users.noreply.github.com>
2022-01-27 13:44:42 -0800

    Fixed parcel
    When committed, fixes the version issues with parcel

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -8,11 +10,7 @@ class MainView extends React.Component {
     constructor() {
         super();
         this.state = {
-            movies: [
-                { _id: 1, Title: 'Ponyo', Description: 'It\'s ponyo! ', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/9/9d/Ponyo_%282008%29.png' },
-                { _id: 2, Title: 'Akira', Description: 'Destruction follows in the wake of Tetsuo as he searches for answers on the mysterious Akira.', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/5/5d/AKIRA_%281988_poster%29.jpg' },
-                { _id: 3, Title: 'Dune', Description: 'Based on the science fiction novel of the same name, set on the planet of Arrakis. ', ImagePath: 'https://upload.wikimedia.org/wikipedia/en/8/8e/Dune_%282021_film%29.jpg' }
-            ],
+            movies: [],
             selectedMovie: null
         }
     }
