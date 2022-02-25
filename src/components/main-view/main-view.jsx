88138f1b6ff340ff72841a6d88aaedd3bf6c569f
TiroV <91702935+TiroV@users.noreply.github.com>
2022-02-24 16:36:52 -0800

    Updated package.json to include redux, added actions.js
    When committed, adds redux and its associated parts, added actions.js folder and imported them into index.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 import axios from 'axios';
 
 import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
-
+import { setMovies } from '../../actions/actions';
+import MoviesList from '../movies-list/movies-list';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
@@ -11,6 +12,7 @@ import { GenreView } from '../genre-view/genre-view';
 import { RegistrationView } from '../registration-view/registration-view';
 import { ProfileView } from '../profile-view/profile-view';
 import { NavbarView } from '../navbar-view/navbar-view';
+
 
 
 import Row from 'react-bootstrap/Row';
